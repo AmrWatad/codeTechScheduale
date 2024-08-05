8f4a3d33ebcd3cb66f575d1dd73c85d47a43518d
mohmad3308 <mohmadmajadly8@gmail.com>
2024-08-05 14:06:59 +0300

    do a 6 service

--- src/main/java/org/institute/Main.java
+++ src/main/java/org/institute/Main.java
@@ -1,15 +1,24 @@
 package org.institute;
 
+import org.institute.Entity.student;
+import org.institute.service.studentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+
+
 @SpringBootApplication
 
 public class Main {
+    @Autowired
+    static studentService st;
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
+        st.addStudent(new student("mohmad","majadly","m1052007", "10/5/2007",true));
     }
 }
 
